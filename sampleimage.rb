2b04210a7c37e3388b86f655ec3cadb04cf47f24
nomuyoshi <nomuyoshi0111@gmail.com>
2020-01-28 07:08:52 +0900

    fix class

--- sampleimage.rb
+++ sampleimage.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class Bprint < Formula
+class Sampleimage < Formula
   desc "sampleimage can create a sample image. Support extentions are .jpeg, .jpg, .png."
   homepage "https://github.com/nomuyoshi/sampleimage"
   url "https://github.com/nomuyoshi/sampleimage/releases/download/0.1.0/sampleimage"
@@ -19,7 +19,7 @@ class Bprint < Formula
     #
     # This test will fail and we won't accept that! For Homebrew/homebrew-core
     # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test bprint`. Options passed
+    # software. Run the test with `brew test sampleimage`. Options passed
     # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
     #
     # The installed folder is not in the path, so use the entire path to any
